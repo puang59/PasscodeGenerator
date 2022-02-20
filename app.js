6794810a9db05a4a10b810fe96472900f0400c72
rezuwii <59052194+rezuwii@users.noreply.github.com>
2022-02-20 21:28:57 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,42 @@
+const button = document.getElementById("gen-btn");
+const pc = document.getElementById("passcode");
+const inputText = document.getElementById("input-txt");
+
+var video = document.getElementById("myVideo");
+var btn = document.getElementById("p-btn");
+
+function code(length) {
+    var result = "";
+    var characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+    var characterLength = characters.length;
+    for (var i = 0; i < length; i++) {
+        result += characters.charAt(Math.floor(Math.random() * characterLength));
+    }
+    return result;
+}
+
+function hasNumber(myString) {
+    return /\d/.test(myString);
+}
+
+button.addEventListener('click', function () {
+    pc.innerText = "";
+    if (hasNumber(inputText.value))
+        pc.append(code(inputText.value));
+    else
+        pc.append("My brain! I think its broken!");
+});
+
+function pauseplay() {
+    if (video.paused) {
+        video.play();
+        btn.innerHTML = "Pause";
+    } else {
+        video.pause();
+        btn.innerHTML = "Play";
+    }
+}
+
+btn.addEventListener('click', function () {
+    pauseplay();
+})
